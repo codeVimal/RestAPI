d91092405cc30a8e8b3d536227cbfe90db2d01e7
codeVimal <95395440+codeVimal@users.noreply.github.com>
2023-09-04 10:25:24 +0530

    Delete DonationAndFundRaisingPlatform directory

--- DonationAndFundRaisingPlatform/src/main/java/com/example/demo/repository/Repo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.DonorEntity;
-
-public interface Repo extends JpaRepository<DonorEntity, Integer>{
-
-}
